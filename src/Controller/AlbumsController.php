03e9fb7282e78d993eb9b373f9075cba075446e7
RaikkoP <raikko.prants@gmail.com>
2024-01-26 12:10:31 +0200

    Created search with POST method

--- src/Controller/AlbumsController.php
+++ src/Controller/AlbumsController.php
@@ -27,4 +27,20 @@ class AlbumsController extends Controller
 		return $this->render($response, 'search.html', ['albums' => $albums,
 			'query' => $query]);
 	}
+
+	public function form(Request $request, Response $response) 
+	{
+		$albums = json_decode(file_get_contents(__DIR__.'/../../data/albums.json'), true);
+
+		$query = $request->getParam('q');
+
+		if($request->isPost()) {
+			$albums = array_values(array_filter($albums, function($album) use ($query){
+				return strpos($album['title'], $query) !== false or strpos($album['artist'], $query) !== false;
+			}));
+		}
+
+		return $this->render($response, 'form.html', ['albums' => $albums,
+			'query' => $query]);
+	}
 }
